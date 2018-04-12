41a106d188a0d6ef97ed56c96af9c556f8833e54
Oscar Martínez <oscar.99.tris@gmail.com>
2018-04-11 21:29:18 -0500

    Cambiar nombre base de datos

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@
         private $dbhost = 'localhost';
         private $dbuser = 'root';
         private $dbpass = 'tristeGDA13##1';
-        private $dbname = 'db_clinica';
+        private $dbname = 'bd_clinica';
         //cnnect
         public function connect ( ){
             $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;";
